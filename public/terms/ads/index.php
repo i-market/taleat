2b00b3374b1927cfa7b83ae0b00b2300e3aa9370
Artem Novikov <novikov@i-market.ru>
2017-06-28 16:03:53 +0300

    remove terms/ads/ page

--- public/terms/ads/index.php
+++
@@ -1,8 +0,0 @@
-<?
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("Согласие на рекламную коммуникацию");
-?>
-
-Text here....
-
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>

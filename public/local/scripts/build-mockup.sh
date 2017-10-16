24b251216b964d661a9012b5261e5a3e1b6a1174
Artem Novikov <novikov@i-market.ru>
2017-10-16 15:26:28 +0300

    npm: mockup build

--- public/local/scripts/build-mockup.sh
+++ public/local/scripts/build-mockup.sh
@@ -4,4 +4,5 @@ pushd mockup &&
 gulp build &&
 mkdir -p ../templates/main/build/assets &&
 rsync -av --exclude='*.html' build/* ../templates/main/build/assets
+rsync -av src/styles/css/* ../templates/main/build/assets/css
 popd

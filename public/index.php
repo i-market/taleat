a753d1bd9b1c96e54715f115a7e86c5b0acc4393
Artem Novikov <novikov@i-market.ru>
2017-10-17 14:53:28 +0300

    homepage news section

--- public/index.php
+++ public/index.php
@@ -279,44 +279,62 @@ $arFiltNew=array("PROPERTY_IS_FEATURED_VALUE" => Iblock::CHECKBOX_TRUE_VALUE);
         "SORT_ORDER2" => "DESC"
     )
 ); ?>
-<section class="news section">
-    <div class="section-title">
-        <div class="wrap">
-            <div class="section-title-block">
-                <h2>Новости</h2>
-                <div class="section-title-link">|<a href="#">все <span class="hidden">новости</span></a></div>
-            </div>
-        </div>
-    </div>
-    <div class="wrap">
-        <div class="grid">
-            <div class="col col-2">
-                <a href="#" class="news-item">
-                    <div class="img">
-                        <img src="<?= v::asset('images/pic/news/1.jpg') ?>" alt="">
-                    </div>
-                    <div class="info">
-                        <p class="date">12.07.2017</p>
-                        <p class="title">Безопасность при ремонте бытовых приборов и техники</p>
-                        <p class="text">Стиральные машины, пылесосы, обогреватели, кондиционеры, холодильники – далеко не полный перечень устройств, без которых жизнь современного горожанина превратилась бы в настоящий ад.</p>
-                    </div>
-                </a>
-            </div>
-            <div class="col col-2">
-                <a href="#" class="news-item">
-                    <div class="img">
-                        <img src="<?= v::asset('images/pic/news/1.jpg') ?>" alt="">
-                    </div>
-                    <div class="info">
-                        <p class="date">12.07.2017</p>
-                        <p class="title">Безопасность при ремонте бытовых приборов и техники</p>
-                        <p class="text">Стиральные машины, пылесосы, обогреватели, кондиционеры, холодильники – далеко не полный перечень устройств, без которых жизнь современного горожанина превратилась бы в настоящий ад.</p>
-                    </div>
-                </a>
-            </div>
-        </div>
-    </div>
-</section>
+<? $APPLICATION->IncludeComponent(
+    "bitrix:news.list",
+    "latest_news_section",
+    Array(
+        "ACTIVE_DATE_FORMAT" => "d.m.Y",
+        "ADD_SECTIONS_CHAIN" => "N",
+        "AJAX_MODE" => "N",
+        "AJAX_OPTION_ADDITIONAL" => "",
+        "AJAX_OPTION_HISTORY" => "N",
+        "AJAX_OPTION_JUMP" => "N",
+        "AJAX_OPTION_STYLE" => "Y",
+        "CACHE_FILTER" => "N",
+        "CACHE_GROUPS" => "Y",
+        "CACHE_TIME" => "36000000",
+        "CACHE_TYPE" => "A",
+        "CHECK_DATES" => "Y",
+        "DETAIL_URL" => "",
+        "DISPLAY_BOTTOM_PAGER" => "Y",
+        "DISPLAY_DATE" => "Y",
+        "DISPLAY_NAME" => "Y",
+        "DISPLAY_PICTURE" => "Y",
+        "DISPLAY_PREVIEW_TEXT" => "Y",
+        "DISPLAY_TOP_PAGER" => "N",
+        "FIELD_CODE" => array("", ""),
+        "FILTER_NAME" => "",
+        "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+        "IBLOCK_ID" => IblockTools::find("news", "news")->id(),
+        "IBLOCK_TYPE" => "news",
+        "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+        "INCLUDE_SUBSECTIONS" => "Y",
+        "MESSAGE_404" => "",
+        "NEWS_COUNT" => 2,
+        "PAGER_BASE_LINK_ENABLE" => "N",
+        "PAGER_DESC_NUMBERING" => "N",
+        "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+        "PAGER_SHOW_ALL" => "N",
+        "PAGER_SHOW_ALWAYS" => "N",
+        "PAGER_TEMPLATE" => ".default",
+        "PAGER_TITLE" => '',
+        "PARENT_SECTION" => "",
+        "PARENT_SECTION_CODE" => "",
+        "PREVIEW_TRUNCATE_LEN" => "",
+        "PROPERTY_CODE" => array("", ""),
+        "SET_BROWSER_TITLE" => "N",
+        "SET_LAST_MODIFIED" => "N",
+        "SET_META_DESCRIPTION" => "N",
+        "SET_META_KEYWORDS" => "N",
+        "SET_STATUS_404" => "N",
+        "SET_TITLE" => "N",
+        "SHOW_404" => "N",
+        "SORT_BY1" => "ACTIVE_FROM",
+        "SORT_BY2" => "SORT",
+        "SORT_ORDER1" => "DESC",
+        "SORT_ORDER2" => "ASC"
+    )
+); ?>
 <section class="useful section hidden">
     <div class="section-title">
         <div class="wrap">
---
+++ public/local/templates/main/components/bitrix/news.list/latest_news_section/template.php
@@ -0,0 +1,38 @@
+<? if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
+
+use App\View as v;
+?>
+<? if (!v::isEmpty($arResult['ITEMS'])): ?>
+    <section class="news section">
+        <div class="section-title">
+            <div class="wrap">
+                <div class="section-title-block">
+                    <h2>Новости</h2>
+                    <div class="section-title-link">|<a href="<?= v::path('news') ?>">все <span class="hidden">новости</span></a></div>
+                </div>
+            </div>
+        </div>
+        <div class="wrap">
+            <div class="grid">
+                <? foreach ($arResult['ITEMS'] as $item): ?>
+                    <div class="col col-2">
+                        <a href="<?= $item['DETAIL_PAGE_URL'] ?>" class="news-item">
+                            <div class="img">
+                                <img src="<?= $item['PREVIEW_PICTURE']['SRC'] ?>" alt="<?= $item['PREVIEW_PICTURE']['ALT'] ?>">
+                            </div>
+                            <div class="info">
+                                <p class="date"><?= $item['DISPLAY_ACTIVE_FROM'] ?></p>
+                                <p class="title"><?= $item['NAME'] ?></p>
+                                <div class="text">
+                                    <div class="editable-area">
+                                        <?= $item['PREVIEW_TEXT'] ?>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                <? endforeach ?>
+            </div>
+        </div>
+    </section>
+<? endif ?>

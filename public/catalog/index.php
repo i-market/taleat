46a19aef30d7451054628e1765f11b64419fae73
Artem Novikov <novikov@i-market.ru>
2017-11-20 13:33:41 +0300

    fix catalog layout duplicate menu

--- public/catalog/index.php
+++ public/catalog/index.php
@@ -1,6 +1,7 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Каталог");
+$APPLICATION->SetPageProperty('layout', 'catalog');
 
 use App\Catalog;
 use App\View as v;

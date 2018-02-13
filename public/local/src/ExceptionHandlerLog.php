75965eb5fbf6f32cb8f56b03a25f25d9facc654a
Artem Novikov <novikov@i-market.ru>
2018-02-13 18:24:12 +0300

    filter exceptions that go to `raven`

--- public/local/src/ExceptionHandlerLog.php
+++ public/local/src/ExceptionHandlerLog.php
@@ -5,13 +5,15 @@ namespace App;
 use Bitrix\Main\Config\Configuration;
 use Raven_Client;
 use Core\Underscore as _;
+use Bitrix\Main\Diag\ExceptionHandlerLog as h;
 
-class ExceptionHandlerLog extends \Bitrix\Main\Diag\ExceptionHandlerLog {
+class ExceptionHandlerLog extends h {
     private $enabled = false;
 
     public function write($exception, $logType) {
-        if ($this->enabled) {
-            App::getInstance()->withRaven(function (\Raven_Client $raven) use ($exception, $logType) {
+        $ignore = [h::LOW_PRIORITY_ERROR, h::ASSERTION, h::IGNORED_ERROR];
+        if ($this->enabled && !in_array($logType, $ignore)) {
+            App::getInstance()->withRaven(function (Raven_Client $raven) use ($exception, $logType) {
                 $raven->captureException($exception, [
                     'logType' => $logType
                 ]);

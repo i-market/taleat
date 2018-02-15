b743f5551f761cece9418234d88d0284f02ccabc
Artem Novikov <novikov@i-market.ru>
2018-02-15 17:11:38 +0300

    bugfix: `name` → `city_name`

--- public/local/templates/main/components/bitrix/sale.ajax.locations/.default_with_filter/result_modifier.php
+++ public/local/templates/main/components/bitrix/sale.ajax.locations/.default_with_filter/result_modifier.php
@@ -1,18 +1,18 @@
 <? if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
 
 // TODO hack: clean up locations
-$uniqueByName = function ($xs) {
+$uniqueBy = function ($xs, callable $f) {
     $keep = [5773]; // locations with dependencies, keep them
     $ret = [];
     $seen = [];
     foreach ($xs as $x) {
-        if (!in_array($x['NAME'], $seen) || in_array($x['ID'], $keep)) {
+        if (!in_array($f($x), $seen) || in_array($x['ID'], $keep)) {
             $ret[] = $x;
         }
-        $seen[] = $x['NAME'];
+        $seen[] = $f($x);
     }
     return $ret;
 };
 
-$arResult['REGION_LIST'] = $uniqueByName($arResult['REGION_LIST']);
-$arResult['CITY_LIST'] = $uniqueByName($arResult['CITY_LIST']);
+$arResult['REGION_LIST'] = $uniqueBy($arResult['REGION_LIST'], function ($x) { return $x['NAME']; });
+$arResult['CITY_LIST'] = $uniqueBy($arResult['CITY_LIST'], function ($x) { return $x['CITY_NAME']; });

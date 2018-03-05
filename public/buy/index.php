9c1c6309552ff084a77152c179e00508d6e20ef8
Artem Novikov <novikov@i-market.ru>
2018-03-05 07:48:07 +0300

    /buy section content (оплата и доставка, разрешение проблем)

--- public/buy/index.php
+++ public/buy/index.php
@@ -1,113 +1,81 @@
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetPageProperty("keywords", "Сетки к бритвам, зубные насадки к электрическим зубным щеткам, запасные части к Браун, Braun, аксессуары к бытовой технике");
 $APPLICATION->SetPageProperty("description", "Мы предлагаем широкий ассортимент аксессуаров  к бытовой технике  фирмы Braun (Браун)");
-$APPLICATION->SetTitle("Оплата/доставка");?><p align="center" style="background: white;">
-</p>
-<h2 align="center" style="text-align: center; background: white;"></h2>
-<p style="text-align: center;">
-</p>
-<p style="text-align: justify;">
- <b><span style="color: #0000ff; font-family: 'Times New Roman', Times; font-size: 14pt;">Оплата&nbsp;</span></b><br>
- <span style="font-family: 'Times New Roman', Times; font-size: 14pt;"> </span>
-</p>
-<p style="text-align: justify;">
- <span style="font-size: 14pt; font-family: 'Times New Roman', Times;">Уважаемый Покупатель, на нашем сайте Вы можете оплатить оформленный заказ следующим образом: </span><br>
- <span style="font-size: 14pt; font-family: 'Times New Roman', Times;">
-	·&nbsp;Сбербанк России (оплата по квитанции через банк, банк онлайн, через любой банкомат или отделение сбербанка). Перевод средств занимает от 1 до 3 банковских дней (как правило, зачисление происходит на следующий день; </span><br>
- <span style="font-size: 14pt; font-family: 'Times New Roman', Times;"> </span><span style="font-size: 14pt;"><span style="font-size: 12pt; font-family: 'Times New Roman', Times;"> </span></span><span style="font-size: 14pt; font-family: 'Times New Roman', Times;">·&nbsp;Яндекс.Касса (оплата банковскими картами VISA, MasterCard &nbsp;и т.д., а так же по средствам электронных денег). Зачисление средств происходит мгновенно.</span><br>
- <span style="font-size: 12pt; font-family: 'Times New Roman', Times;"> </span><span style="font-size: 14pt; font-family: 'Times New Roman', Times;">Оплата товара осуществляется после оформления заказа.&nbsp; </span><br>
- <span style="font-size: 14pt; font-family: 'Times New Roman', Times;">
-	Менеджер интернет магазина подтвердит наличие товара, обработает Ваш заказ, после чего Вам придет уведомление по электронной почте.</span><br>
- <span style="font-size: 14pt; font-family: 'Times New Roman', Times;">
-	Оплата производится ТОЛЬКО по безналичному расчету на расчетный счет ООО «Талеат-Сервис». </span><br>
- <span style="font-size: 14pt; font-family: 'Times New Roman', Times;">
-	Формы оплаты: наличными курьеру, на личные карты сбербанка не производим. &nbsp;</span>
-</p>
-<p style="text-align: justify;">
- <span style="font-size: 14pt;"> <span style="font-family: 'Times New Roman', Times;"> </span></span><span style="font-family: 'Times New Roman', Times; font-size: 14pt;"> </span>
-</p>
-<p style="text-align: justify;">
- <span style="font-family: 'Times New Roman', Times; font-size: 14pt;"> </span><span style="color: #0000ff;"><b><span style="font-family: 'Times New Roman', Times; font-size: 14pt;">Доставка</span></b></span><span style="font-family: 'Times New Roman', Times; font-size: 14pt;"> </span>
-</p>
-<p style="text-align: justify;">
- <span style="font-family: 'Times New Roman', Times; font-size: 14pt;"> </span><span style="font-size: 14pt; font-family: 'Times New Roman', Times;">Отправка вашего заказа осуществляется в день поступления оплаты на расчетный счет или на следующий рабочий день. Все отправления Отправка заказов осуществляется&nbsp;только через курьерскую службу <a href="http://www.emspost.ru/ru/">EMS</a>.</span><br>
- <span style="font-size: 14pt; font-family: 'Times New Roman', Times;">
-	На данный момент мы не работаем с Почтой России.</span><br>
- <span style="font-size: 14pt; font-family: 'Times New Roman', Times;"> </span><br>
- <span style="font-size: 14pt; font-family: 'Times New Roman', Times;">
-	Почему мы не отправляем наложенным платежом можете прочитать тут.&nbsp;</span><br>
- <span style="font-family: 'Times New Roman', Times; font-size: 14pt;"> </span><span style="font-size: 14pt; font-family: 'Times New Roman', Times;"> </span><span style="font-family: 'Times New Roman', Times; font-size: 14pt;"> </span>
-</p>
-<p style="text-align: justify;">
- <span style="font-family: 'Times New Roman', Times; font-size: 14pt;"> </span><br>
- <span style="font-family: 'Times New Roman', Times; font-size: 14pt;"> </span><b><span style="color: #0000ff; font-family: 'Times New Roman', Times; font-size: 14pt;">Оформление заказа</span></b><span style="font-family: 'Times New Roman', Times; font-size: 14pt;"> </span>
-</p>
-<p style="text-align: justify;">
- <span style="font-family: 'Times New Roman', Times; font-size: 14pt;"> </span>
-</p>
-<p style="text-align: justify;">
- <span style="font-size: 14pt; font-family: 'Times New Roman', Times;"><span style="font-family: 'Times New Roman', Times;">И<span style="font-family: 'Times New Roman', Times;">нтернет-магазин работает с понедельника по пятницу с 09:00 до 18:00.Порядок оформления заказа в интернет-магазине Талеат: </span></span></span><br>
- <span style="font-family: 'Times New Roman', Times; font-size: 14pt;"> </span><span style="font-size: 14pt; font-family: 'Times New Roman', Times;"> </span><span style="font-size: 12pt; font-family: 'Times New Roman', Times;"><span style="font-family: 'Times New Roman', Times; font-size: 14pt;">1. Выбираете необходимый товар, который переместится в корзину. Обязательно проверьте тип Вашего изделия, подходит ли данная зап.часть к вашему прибору</span></span>
-</p>
-<p style="text-align: justify;">
-</p>
- <span style="font-size: 12pt; font-family: 'Times New Roman', Times;"><br>
- </span><br>
- <img width="314" alt="купить.png" src="/upload/medialibrary/6e1/kupit.png" height="155" title="купить.png"><br>
- <span style="font-size: 14pt;"><br>
- <span style="font-family: 'Times New Roman', Times;"><a href="http://www.service.braun.com/product/where_model_number.asp?rg=EU&c=RU&l=RU&typ=wmn&s=&seite=&sString=">Как найти модель/тип изделия Браун (Braun)?</a></span></span><br>
- <span style="font-family: 'Times New Roman', Times; font-size: 14pt;"> </span><span style="font-size: 14pt; font-family: 'Times New Roman', Times;"> <a href="http://www.service.braun.com/home.asp?rg=EU&c=RU&l=RU&typ=pf&s=&seite=&sString=">Список запасных частей и аксессуаров к вашему прибору можете проверить на сайте Braun</a></span><span style="font-size: 14pt;"><br>
- </span><span style="font-size: 14pt;"><br>
- <span style="font-family: 'Times New Roman', Times;"> </span></span><span style="font-size: 12pt; font-family: 'Times New Roman', Times;"><span style="font-family: 'Times New Roman', Times; font-size: 14pt;">2. После выбора необходимого товара перейдите в корзину для дальнейшего оформления заказа (правый верхний угол страницы).</span><br>
- </span><span style="font-size: 14pt;"><br>
- </span><img width="314" alt="корзина.png" src="/upload/medialibrary/a84/korzina.png" height="191" title="корзина.png"><span style="font-size: 14pt;"><br>
- </span><span style="font-size: 14pt; font-family: 'Times New Roman', Times;">3. Проверьте, все ли правильно Вы выбрали к заказу и нажмите "оформить заказ"</span><br>
- <img width="314" alt="оформить заказ.png" src="/upload/medialibrary/55a/oformit-zakaz.png" height="154" title="оформить заказ.png"><br>
- <span style="font-size: 12pt;">
-<p style="text-align: justify;">
-	 4. Внесите свои данные, обязательно адрес для доставки, контактный телефон и адрес эл.почты, куда будут поступать все информационные уведомления
-</p>
- </span><br>
- <img width="287" alt="ФИО.png" src="/upload/medialibrary/482/fio.png" height="235" title="ФИО.png"><span style="font-size: 14pt;"><br>
- </span><span style="font-size: 14pt; font-family: 'Times New Roman', Times;">5. Следующий пункт "способ оплаты". На данный момент есть два основных способа оплатить Ваш заказ:<br>
- </span><br>
- <img width="314" alt="форма оплаты.png" src="/upload/medialibrary/6bd/forma-oplaty.png" height="90" title="форма оплаты.png"><span style="font-size: 14pt;"><br>
- </span>
-<p style="text-align: justify;">
- <span style="font-size: 14pt; font-family: 'Times New Roman', Times;">- "квитанция Сбербанк" (оплата поступает, как правило, на следующий день);</span><br>
- <span style="font-family: 'Times New Roman', Times; font-size: 14pt;"> </span><span style="font-size: 14pt; font-family: 'Times New Roman', Times;">- &nbsp;Яндекс Касса (оплата электронными кошельками и картами, оплата поступает сразу же) </span><br>
- <span style="font-family: 'Times New Roman', Times; font-size: 14pt;"> </span><span style="font-size: 14pt;"><br>
- <span style="font-family: 'Times New Roman', Times;"> </span><span style="font-family: 'Times New Roman', Times; font-size: 12pt;">
-	&nbsp;</span></span><span style="font-size: 14pt; font-family: 'Times New Roman', Times;">На этом все. Теперь остается дождаться обработки заказа и письма от нашего менеджера, о состоянии заказа, стоимости и сроках доставки. После чего Вы сможете войти в личный кабинет и получить доступ к оплате вашего заказа.</span><br>
- <span style="font-family: 'Times New Roman', Times; font-size: 14pt;"> </span><span style="font-size: 14pt; font-family: 'Times New Roman', Times;"> </span><br>
- <span style="font-family: 'Times New Roman', Times; font-size: 14pt;"> </span><span style="font-size: 14pt; font-family: 'Times New Roman', Times;">
-	Если у Вас возникли сложности с заказом на нашем сайте, Вы обнаружили неверно указанную информацию или возникли какие-то вопросы, свяжитесь с нами мы постараемся Вам помочь: </span><span style="font-family: 'Times New Roman', Times; font-size: 14pt;"> </span><br>
- <span style="font-family: 'Times New Roman', Times; font-size: 14pt;"> </span>
-</p>
-<p style="text-align: center;">
- <span style="font-size: 16pt; color: #0000ff; font-family: 'Times New Roman', Times;"><b>С уважением, ООО "Талеат-Сервис"</b></span>
-</p>
-<p style="text-align: center;">
- <span style="font-size: 16pt; color: #0000ff; font-family: 'Times New Roman', Times;"><b class="text-nowrap">+7 (495) 018-59-47 </b></span><br>
- <b> </b><span style="font-family: 'Times New Roman', Times;"><b> </b></span><span style="font-size: 16pt; font-family: 'Times New Roman', Times;"><b> </b></span><span style="color: #0000ff; font-size: 16pt; font-family: 'Times New Roman', Times;"><b> </b></span><span style="font-size: 14pt;"><span style="color: #0000ff; font-size: 16pt; font-family: 'Times New Roman', Times;"><b> </b></span><a href="mailto:asn@taleat.ru"><span style="color: #0000ff; font-size: 16pt; font-family: 'Times New Roman', Times;"><b>asn@taleat.ru</b></span></a><span style="color: #0000ff; font-size: 16pt; font-family: 'Times New Roman', Times;"><b> </b></span></span><br>
- <br>
+$APPLICATION->SetTitle("Оплата/доставка");?><p>
+ <b><u>Оплата</u></b>
 </p>
 <p>
-</p>
-<p style="text-align: center;">
-</p>
-<div>
-</div>
-<h3>
-<p align="center" style="background: white;">
-</p>
- </h3>
-<h3> </h3>
-<p align="center" style="background: white;">
-</p>
-<p align="center" style="background: white;">
-</p>
-<p align="center" style="background: white;">
+	 Уважаемый Покупатель, на нашем сайте Вы можете оплатить оформленный заказ следующим образом:
 </p>
 <p>
+	 -&nbsp;Сбербанк России (оплата по квитанции через банк, банк онлайн, через любой банкомат или отделение сбербанка). Перевод средств занимает от 1 до 3 банковских дней (как правило, зачисление происходит на следующий банковский день).
+</p>
+<p>
+ <b><span style="color: red;">- Оплата картами на данный момент недоступна.</span></b>
+</p>
+<p>
+	 Оплата товара осуществляется после оформления заказа.
+</p>
+<p>
+	 Менеджер интернет магазина подтвердит наличие товара, обработает Ваш заказ, после чего Вам придет уведомление по электронной почте.
+</p>
+<p>
+	 Оплата производится ТОЛЬКО по безналичному расчету на расчетный счет ООО «Талеат-Сервис».
+</p>
+<p>
+ <b><span style="color: red;">ОПЛАТА НА КАРТУ</span></b> переводом с вашей карты или как-то еще – это экономическое преступление – мы так не работаем. Так же напоминаем, что данная операция может вызвать преследование вас, как пособника экономическому преступлению.
+</p>
+<p>
+	 Примеры такие уже были и не раз!!!
+</p>
+<p>
+ <b><span style="color: red;">&nbsp;</span></b>
+</p>
+<p>
+ <b><span style="color: #0070c0;">Если по каким-либо причинам Вы решили отказаться от приобретенного товара, перейдите на страницу <a href="/buy/issue-resolution/" style="color: red;">Решение проблем</a>.</span></b>
+</p>
+<p>
+ <b><u> </u></b>
+</p>
+<p>
+ <b><u>Доставка</u></b>
+</p>
+<p>
+	 Доставка курьерской службой осуществляется по Москве и Санкт-Петербургу.
+</p>
+<p>
+	 Москва – 300 руб. (срок доставки 1-2 дня)
+</p>
+<p>
+	 Санкт-Петербург – 450 руб. (срок доставки 1-2 дня)
+</p>
+<p>
+	 &nbsp;
+</p>
+<p>
+	 В остальные регионы России отправляем Почтой России 1 классом.
+</p>
+<p>
+	 Стоимость доставки – 350 руб.
+</p>
+<p>
+	 Отправка посылок Почтой России производиться по вторникам и пятницам.
+</p>
+<p>
+	 Почты России - это стабильный и надежный вариант – кто бы, что про него ни говорил. В среднем доставка занимает 10 суток с момента отправки (или 8 РАБОЧИХ дней). Доставка реально по ВСЕЙ России. Очень важно указать Ваш полный почтовый адрес и индекс – это снизить задержки посылки.
+</p>
+<p>
+	 &nbsp;
+</p>
+<p>
+	 Если у Вас возникли сложности с заказом на нашем сайте, Вы обнаружили неверно указанную информацию или возникли какие-то вопросы, свяжитесь с нами, мы постараемся Вам помочь:
+</p>
+<p>
+ <b>&nbsp;</b>
+</p>
+<p align="center">
+ <b>С уважением, ООО "Талеат-Сервис"</b>
+</p>
+<p align="center">
+ <a href="mailto:asn@taleat.ru"><b><span style="color: windowtext;">asn@taleat.ru</span></b></a>
 </p><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>

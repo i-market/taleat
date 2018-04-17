077a750479ebd03864d38d1fa20c317d2aa2a6b7
Artem Novikov <novikov@i-market.ru>
2018-04-17 14:43:06 +0300

    change content

--- public/include/layout/mega_menu/reception.php
+++ public/include/layout/mega_menu/reception.php
@@ -1,1 +1,4 @@
-Наши приемные пункты в ремонт, а так же пункты продажи запасных частей и аксессуаров к бытовой технике фирм Braun и Babyliss PARIS
+<div>
+ <span style="font-family: &quot;Times New Roman&quot;, Times; font-size: 12pt;">Наши приемные пункты в ремонт, а так же пункты продажи запасных частей и аксессуаров к бытовой технике фирм: <b>Braun, Delonghi, Kenwood, Ariete,&nbsp;Babyliss&nbsp;Paris, Philips, Saeco, Avent,&nbsp;Sonicare, Moser, Wahl, Ermila,&nbsp;Panda, Gutrend,&nbsp;Wella (GHD, Londa, Sebastian, Nioxin, SP, Systemprofessional, OPI),&nbsp;Hapica, Asahi Irica, Solstick, Watershot, Рокимед.</b></span>
+</div>
+ <br>
--- public/priemnie-punkti/index.php
+++ public/priemnie-punkti/index.php
@@ -2,18 +2,17 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Приемные пункты");
 ?><p class="section-block-text" style="text-align: center;">
- <b><span style="color: #0000ff;">Наши приемные пункты в ремонт и продажи запасных частей и аксессуаров к бытовой технике&nbsp;&nbsp;фирм:</span></b>
+ <b><span style="color: #0000ff;">Наши приемные пункты в ремонт, а так же пункты продажи запасных частей и аксессуаров к бытовой технике фирм:</span></b>
 </p>
 <p class="section-block-text" style="text-align: center;">
- <b><span style="color: #0000ff;">BRAUN (Браун), BABYLISS Paris (Бэбилис), DELONGHI (Делонги), KENWOOD (Кенвуд), WAHL (Уолл), MOSER (Мозер)</span></b>
+	<b><span style="color: #0000ff;">Braun, Delonghi, Kenwood, Ariete,&nbsp;Babyliss&nbsp;Paris, Philips, Saeco, Avent,&nbsp;Sonicare, Moser, Wahl, Ermila,&nbsp;Panda, Gutrend,&nbsp;Wella (GHD, Londa, Sebastian, Nioxin, SP, Systemprofessional, OPI),&nbsp;Hapica, Asahi Irica, Solstick, Watershot, Рокимед.</span></b>
 </p>
-<p class="section-block-text" style="text-align: center;">
-</p>
-<p class="section-block-text" style="text-align: center;">
- <b>"Сервисный центр на Селезневке"&nbsp;</b>
+<p style="text-align: center;">
+ <b>Сервисный центр на Селезневке"</b><br>
+ <b><span style="color: #0000ff;">Продажа запасных частей и пункт приема техники в ремонт</span></b><br>
+ <b> </b><b> <span style="color: #ff0000;">Цены в розничных пунктах могут отличаться от цен, указанных в интернет-магазине!</span></b>
 </p>
 <div style="text-align: center;">
- <span style="color: #ff0000;"><b>Только пункт приема техники в ремонт!</b></span><br>
  <b>
 	м. Достоевская, м Новослободская</b><br>
  <b>г. Москва, ул. Селезневская, д. 30, корп. 1</b><br>
@@ -44,7 +43,7 @@ $APPLICATION->SetTitle("Приемные пункты");
 				 Москва, ул. Барклая, д. 8, 4 этаж, павильон 427 <br>
 				 телефон: (495) 730-00-06 (доб. 427) <br>
 				 (495) 960-53-15&nbsp;<br>
-				 с 11:00 до 21:00 <br>
+				 с 11:00 до 20:00 <br>
 				 без выходных</strong>
 			</div>
 			<div align="center">
@@ -72,7 +71,7 @@ $APPLICATION->SetTitle("Приемные пункты");
 				 м. Пражская <br>
 				 Москва, ул. Кировоградская, д.15, цокольный этаж, павильон 1П/45-46&nbsp;<br>
 				 телефон: (495) 518-66-89<br>
-				 с 11:00 до 20:00 <br>
+				 с 11:00 до 19:00 <br>
 				 без выходных </strong> <br>
  <img width="480" alt="Прага.jpeg" src="/upload/medialibrary/c9d/c9dda335d8da5493d1c21f25f3f1fed2.jpeg" height="302" title="Прага.jpeg" border="0" align="middle"> <br>
  <br>

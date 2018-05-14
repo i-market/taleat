9602f6ce3452a508fab92613abe69792e46eda27
Artem Novikov <novikov@i-market.ru>
2018-05-14 11:55:37 +0300

    remove imarket:system.auth.* components

--- public/partneram/auth/index.php
+++ public/partneram/auth/index.php
@@ -3,20 +3,8 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Авторизация");
 ?>
 <?
-use App\App;
 use App\View as v;
 
-App::getInstance()->assert(false, 'deprecated page');
 LocalRedirect(v::path('partneram'));
 ?>
-<?$APPLICATION->IncludeComponent(
-   "imarket:system.auth.authorize",
-   "",
-   Array(
-      "REGISTER_URL" => "",
-      "PROFILE_URL" => "",
-      "SHOW_ERRORS" => "Y"
-   ),
-false
-);?>
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
--- public/partneram/forgot/index.php
+++
@@ -1,8 +0,0 @@
-<?
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("Восстановление пароля");
-?><?$APPLICATION->IncludeComponent(
-"imarket:system.auth.forgotpasswd",
-".default",
-Array()
-);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
